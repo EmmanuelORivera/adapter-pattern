f1ad9f815f467c837f561eb4cb77b87dfff79a51
Emmanuel Ochoa <eorivera95@gmail.com>
2024-05-08 19:18:04 -0600

    an example of adapter pattern

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// Structural design pattern that allows objects with incompatible interfaces
+// to collaborate.
+Adaptee adaptee1 = new Adaptee();
+ITarget target = new Adapter(adaptee1);
+
+System.Console.WriteLine("Adaptee interface is incompatible with the client.");
+System.Console.WriteLine("But with adapter client can call it's method.");
+
+System.Console.WriteLine(target.GetRequest());
+
+public interface ITarget
+{
+    string GetRequest();
+}
+
+class Adaptee
+{
+    public string GetSpecificRequest()
+    {
+        return "Specific request";
+    }
+}
+
+class Adapter : ITarget
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        this._adaptee = adaptee;
+    }
+
+    public string GetRequest()
+    {
+        return $"This is {this._adaptee.GetSpecificRequest()}";
+    }
+}
+
